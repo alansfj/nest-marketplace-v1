b039db50244c6552af6a51977195d38ae7506a83
Alan Soto <alansfj348@gmail.com>
2025-12-15 12:46:46 -0600

    removed comment

--- src/types/store/store.repository.interface.ts
+++ src/types/store/store.repository.interface.ts
@@ -1,9 +1,4 @@
 import { Store } from 'src/entities/store.entity';
 import { IBaseTypeormRepository } from '../base-typeorm.repository.interface';
 
-export abstract class IStoreRepository extends IBaseTypeormRepository<Store> {
-  // abstract findOneEqualBy<T extends StoreSelectableColumns>(
-  //   options: Partial<Record<StoreSelectableColumns, PrimitiveColumns>>,
-  //   select?: T[],
-  // ): Promise<Pick<Store, T> | null>;
-}
+export abstract class IStoreRepository extends IBaseTypeormRepository<Store> {}
