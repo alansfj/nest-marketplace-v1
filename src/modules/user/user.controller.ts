1dd0d484d2c1720ce64956f3da74d70bef1b1749
Alan Soto <alansfj348@gmail.com>
2025-12-16 12:44:46 -0600

    removed unused endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { IUserService } from 'src/types/user/user.service.interface';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: IUserService) {}
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOneById(+id);
-  }
 }
